8015513ad7f073303c5a19bad3001189ccef3594
Daniel Ângelo <danielangelo1234@gmail.com>
2023-10-31 17:15:39 -0300

    feat: add modules js

---
+++ js/modules/donwloadGrid.js
@@ -0,0 +1,18 @@
+export default function initDonwloadGrid() {
+  function downloadGrid() {
+    const generatedImage = document.querySelector(".generated-image");
+    if (generatedImage) {
+      const link = document.createElement("a");
+      link.href = generatedImage.src;
+      link.download = "semaninha.png";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } else {
+      alert("Sem imagem para baixar!");
+    }
+  }
+
+  const downloadButton = document.getElementById("downloadButton");
+  downloadButton.addEventListener("click", downloadGrid);
+}
