515786613fb5aa0082d15e3e6bc760e6232bc991
danielangelo1 <danielangelo1234@gmail.com>
2025-08-14 01:16:29 -0300

    chore: configure test environment and remove eslint comment from Canvas component

--- config/vite.config.ts
+++ config/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "/Users/danielangelo/Desktop/projetos/semaninha-lastfm/tests/setup.ts",
+    setupFiles: ["tests/setup.ts"],
     css: true,
     coverage: {
       provider: "v8",
